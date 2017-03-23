8e0b24f9ef45e2e198ce9765c1c4ed4c340072d4
Jan Aare van Gent <van_gentjanaare@hotmail.com>
2017-03-23 15:49:32 +0200

    pannkoogid tartus

--- hello/models.py
+++ hello/models.py
@@ -3,3 +3,9 @@ from django.db import models
 # Create your models here.
 class Greeting(models.Model):
     when = models.DateTimeField('date created', auto_now_add=True)
+
+class Petition(models.Model):
+    title_text = models.CharField(max_length=50, default='Pankooke Tartusse')
+    pub_date = models.DateTimeField('date published', auto_now_add=True)
+    votes = models.IntegerField(default=0)
+    desc_text = models.CharField(max_length=1000, default='Oleks tore kui saaks rohkem pannkooke')
--- hello/views.py
+++ hello/views.py
@@ -2,7 +2,7 @@ import requests
 from django.http import HttpResponse
 from django.shortcuts import render
 
-from .models import Greeting
+from .models import Greeting, Petition
 
 
 # Create your views here.
@@ -18,7 +18,10 @@ def index(request):
 def db(request):
     greeting = Greeting()
     greeting.save()
-
     greetings = Greeting.objects.all()
 
-    return render(request, 'db.html', {'greetings': greetings})
+    petition = Petition()
+    petition.save()
+    petitions = Petition.objects.all()
+
+    return render(request, 'db.html', {'greetings': greetings, 'petitions': petitions})
