112b4715dc148443317ff36731e6a1b7e8fa2422
Janmoppel <moppel.jan@gmail.com>
2017-04-13 14:11:40 +0300

    Try4

--- hello/models.py
+++ hello/models.py
@@ -7,7 +7,7 @@ class Greeting(models.Model):
 
 
 class Petition(models.Model):
-    title_text = models.CharField(max_length=50)
+    title_text = models.CharField(max_length=50, default='Increase')
     pub_date = models.DateTimeField('date published', auto_now_add=True)
     votes = models.IntegerField(default=0)
     desc_text = models.CharField(max_length=1000)
