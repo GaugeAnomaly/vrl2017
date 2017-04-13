51cf623f6c9433150a0171d0d73008a4c4812452
Janmoppel <moppel.jan@gmail.com>
2017-04-13 14:16:55 +0300

    Try5

--- hello/models.py
+++ hello/models.py
@@ -7,12 +7,12 @@ class Greeting(models.Model):
 
 
 class Petition(models.Model):
-    title_text = models.CharField(max_length=50, default='Increase')
+    title_text = models.CharField(max_length=50, default='Increase amount of parking lots near Philosophicum')
     pub_date = models.DateTimeField('date published', auto_now_add=True)
     votes = models.IntegerField(default=0)
-    desc_text = models.CharField(max_length=1000)
-    improvement = models.CharField(max_length=1000)
-    picture = models.ImageField(default='hello/static/images/anatoomikum.jpg')
+    desc_text = models.CharField(max_length=1000, default='I do not have a place to park my bike')
+    improvement = models.CharField(max_length=1000, default='We will all have a place for our bikes!')
+    picture = models.ImageField(default='.../static/images/anatoomikum.jpg')
 
     def __str__(self):
         return self.title_text
