11b44cf65e04e841b11c3d3d685754466e2c4731
Janmoppel <moppel.jan@gmail.com>
2017-04-13 14:35:21 +0300

    Try6

--- hello/models.py
+++ hello/models.py
@@ -10,7 +10,7 @@ class Petition(models.Model):
     title_text = models.CharField(max_length=50, default='Increase amount of parking lots near Philosophicum')
     pub_date = models.DateTimeField('date published', auto_now_add=True)
     votes = models.IntegerField(default=0)
-    desc_text = models.CharField(max_length=1000, default='I do not have a place to park my bike')
+    desc_text = models.CharField(max_length=1000, default='I cannot park my bike!')
     improvement = models.CharField(max_length=1000, default='We will all have a place for our bikes!')
     picture = models.ImageField(default='.../static/images/anatoomikum.jpg')
 
